d0f71cd889c4993f1c2ae99bb7b5b1e8a50960fc
Siyuan Gao <sgao@mobify.com>
2016-07-22 10:30:11 -0700

    Adding geocode api

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 var HTTPClient =  require('request-promise');
+var Promise = require('bluebird');
+var utf8 = require('utf8');
 var MongoClient = require('mongodb').MongoClient;
+var format = require("string-template")
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3939;
 var APIURI = 'http://api.p2pquake.com/v1/human-readable';
-var GeocodeAPI = 'http://nominatim.openstreetmap.org/search?q=<% city %>&format=json';
+var GeocodeAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address={addr}&key=' + process.env.GOOGLE_API;
+
+function startPulling(collection) {
+    setInterval(function() {
+        HTTPClient(APIURI).then(function (response) {
+            var data = JSON.parse(response);
+            // Filter out non earchquake activities
+            data = data.filter(function (dataEntry) {
+                return dataEntry.earthquake;
+            });
+            Promise.all(data.map(function (entry) {
+                return Promise.all(entry.points.map(function (point) {
+                    var url = format(GeocodeAPI, {addr: utf8.encode(point.addr)});
+                    return HTTPClient(url).then(function (value) {
+                        point.geocode = JSON.parse(value);
+                    });
+                }));
+            })).then(function () {
+                collection.insertMany(data, function (err, result) {
+                    if(!!result.nInserted) {
+                        console.log(`inserted ${result.nInserted} documents`);
+                    }
+                });
+            });
+        });
+    }, 10000);
+}
 
 MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     console.log("Connected correctly to server");
     var collection = db.collection('quakes');
-    setInterval(function() {
-        HTTPClient(APIURI).then(function (response) {
-            var data = JSON.parse(response);
-            collection.insertMany(data, function (err, result) {
-                if(!!result.nInserted) {
-                    console.log(`inserted ${result.nInserted} documents`);
-                }
-            });
-        });
-    }, 60000);
+    startPulling(collection);
 });
 
 app.get('/', function (req, res) {
